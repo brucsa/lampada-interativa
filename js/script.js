26a60253125b273cf4f75f1459f6b3a9341a1ef5
Bruna Almeida <bru.csa16@gmail.com>
2025-04-25 09:14:32 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 let fundo = document.body
 
 function inicio() {
-    fundo.style.background = "#111"
-
+    fundo.style.background = "#000"
     btnAcender.style.display = "inline" //aparece
     btnApagar.style.display = "none" //desaparece
     btnTrocar.style.display = "none" //desaparece
     btnQuebrar.style.display = "none" //desaparece
+    btnVoltar.style.display = "none" //desaparece
 }
 
 function acender() {
@@ -17,38 +17,54 @@ function acender() {
     btnApagar.style.display = "inline" //aparece
     btnTrocar.style.display = "none" //desaparece
     btnQuebrar.style.display = "none" //desaparece
+    btnVoltar.style.display = "none" //desaparece
 }
 btnAcender.addEventListener("click", acender)
 
 function apagar() {
     imgLampada.setAttribute("src", "imagens/lampada-off.png")
-    fundo.style.background = "#111"
+ fundo.style.background = "#3F2144"
 
     btnAcender.style.display = "none" //desaparece
     btnApagar.style.display = "none" //desaparece
     btnTrocar.style.display = "none" //desaperece
     btnQuebrar.style.display = "inline" //aparece
+    btnVoltar.style.display = "none" //desaparece
 }
 btnApagar.addEventListener("click", apagar)
 
 function quebrar() {
     imgLampada.setAttribute("src", "imagens/lampada-quebrada.png")
-    fundo.style.background = "#111"
+    fundo.style.background = "#000"
 
     btnAcender.style.display = "none" //desaparece
     btnApagar.style.display = "none" //desaparece
     btnQuebrar.style.display = "none" //desaparece
     btnTrocar.style.display = "inline" //aparece
+    btnVoltar.style.display = "none" //desaparece
 }
 imgLampada.addEventListener("dblclick", quebrar)
 
 function trocar() {
     imgLampada.setAttribute("src", "imagens/lampada-off.png")
-    fundo.style.background = "#111"
 
-    btnAcender.style.display = "inline" //desaparece
+    btnAcender.style.display = "none" //desaparece
     btnApagar.style.display = "none" //desaparece
     btnQuebrar.style.display = "none" //desaparece
-    btnTrocar.style.display = "none" //aparece
+    btnTrocar.style.display = "none" //desaparece
+    btnVoltar.style.display = "inline" //aparece
+
 }
 btnTrocar.addEventListener("click", trocar)
+
+function voltar() {
+    imgLampada.setAttribute("src", "imagens/lampada-inicio.png")
+    fundo.style.background = "#000"
+
+    btnAcender.style.display = "inline" //aparece
+    btnApagar.style.display = "none" //desaparece
+    btnTrocar.style.display = "none" //desaparece
+    btnQuebrar.style.display = "none" //desaparece
+    btnVoltar.style.display = "none" //desaparece
+}
+btnTrocar.addEventListener("click", voltar)
